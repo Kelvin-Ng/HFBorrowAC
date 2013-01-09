14ec7e4ae7eeb7cb511dca065a0d9a03c81fe982
Kelvin <kelvin9302104@gmail.com>
2013-01-09 15:00:07 +0800

    Fix minor mistake

--- install.sql
+++ install.sql
@@ -1,9 +1,9 @@
 CREATE TABLE borrow (
-	username VARCHAR,
-	provider VARCHAR,
+	username VARCHAR(256),
+	provider VARCHAR(256),
 	blacklist BOOLEAN
 	);
 
 CREATE TABLE provide (
-	id VARCHAR(256),
+	id VARCHAR(256)
 	);
--- locale.php
+++ locale.php
@@ -13,13 +13,13 @@ function use_locale($locale)
 if (isset($_GET['locale']))
 {
 	setcookie('locale', $_GET['locale'], time() + 3600 * 24);
-	use_locale($locale_code($_GET['locale']));
+	use_locale($locale_code[$_GET['locale']]);
 }
 else if (!isset($_COOKIE['locale']))
 {
 	$locale = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
 	setcookie('locale', $locale[0], time() + 3600 * 24 * 7);
-	use_locale($locale_code($locale));
+	use_locale($locale_code[$locale]);
 }
 else
 {
